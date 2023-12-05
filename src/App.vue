b217a259cfdcfe85c08b63813152ca5d4758cab7
ThomasP <legends.legardus@gmail.com>
2023-12-05 10:14:52 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import MapOsm from '../src/components/MapOsm.vue';
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink class="px-2" to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+      <div class="container-fluid">
+        <h1> Open Street Map - Carte</h1>
+        <MapOsm/>
+    </div>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
